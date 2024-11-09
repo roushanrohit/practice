10d186783064de0901e46bc7d690b64876841569
rohit-roushan-novopay <rohit.roushan@novopay.in>
2024-11-09 15:24:27 +0530

    minimum steps to 1

--- src/dp/DPUse.java
+++ src/dp/DPUse.java
@@ -7,12 +7,86 @@ public class DPUse {
 
     public static void main(String[] args) {
 
-        int n = 45;
+        int n = 5;
         //int n = 500000;
         Date startTime = new Date();
         System.out.println(n + "th Fibonacci Number is: " + fibonacci(n));
         Date endTime = new Date();
         System.out.println("Time taken: " + (endTime.getTime() - startTime.getTime()));
+
+        int x = 1000;
+        System.out.println("Minimum steps to reach 1: " + minStepsTo1Memoization(x));
+
+
+    }
+
+    private static int minStepsTo1Dp(int x) {
+
+        int[] storage = new int[x + 1];
+        storage[1] = 0;
+
+        for(int i = 2; i <= x; i++){
+            int op1 = 1 + storage[i-1];
+            int op2 = Integer.MAX_VALUE;
+            int op3 = Integer.MAX_VALUE;
+            if(i % 2 == 0){
+                op2 = 1 + storage[i/2];
+            }
+            if(i % 3 == 0){
+                op3 = 1 + storage[i/3];
+            }
+            storage[i] = Math.min(op1, Math.min(op2, op3));
+        }
+
+        return storage[x];
+    }
+
+    private static int minStepsTo1(int x) {
+
+        // edge case
+        if(x < 1) return -1;
+
+        // base case
+        if(x == 1) return 0;
+
+        int op1 = Integer.MAX_VALUE;
+        int op2 = Integer.MAX_VALUE;
+        if(x % 3 == 0) op1 = 1 + minStepsTo1(x/3);
+        if(x % 2 == 0) op2 = 1 + minStepsTo1(x/2);
+        int op3 = 1 + minStepsTo1(x - 1);
+
+        return Math.min(op3, Math.min(op1, op2));
+    }
+
+    private static int minStepsTo1Memoization(int x) {
+
+        int[] storage = new int[x + 1];
+        for(int i = 0; i <= x; i++) storage[i] = -1;
+        return minStepsTo1Memoization(x, storage);
+    }
+
+    private static int minStepsTo1Memoization(int x, int[] storage) {
+
+        // base case
+        if(x == 1) {
+            storage[1] = 0;
+            return storage[1];
+        }
+
+        if(storage[x] != -1) return storage[x];
+
+        int op1 = Integer.MAX_VALUE;
+        int op2 = Integer.MAX_VALUE;
+        if(x % 3 == 0) {
+            op1 = 1 + minStepsTo1Memoization(x / 3);
+        }
+        if(x % 2 == 0) {
+            op2 = 1 + minStepsTo1Memoization(x / 2);
+        }
+        int op3 = 1 + minStepsTo1Memoization(x - 1);
+
+        storage[x] =  Math.min(op3, Math.min(op1, op2));
+        return storage[x];
     }
 
     private static BigInteger fibonacci(int n) {
@@ -24,6 +98,7 @@ public class DPUse {
         return fibonacci(n, storage);
     }
 
+    // O(N) -- Both Time and Space Complexity
     private static BigInteger fibonacci(int n, BigInteger[] storage) {
 
         // base case
